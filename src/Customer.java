a7cd444d7e7488e5da93b57f80ed57d823dd02c5
trung1609 <vmt162005@gmail.com>
2025-12-16 13:29:17 +0700

    xuatsac1

---
+++ src/Customer.java
@@ -0,0 +1,7 @@
+public class Customer {
+    public int id;
+    public String name;
+    public String email;
+
+    public void showInfo(){}
+}
---
+++ src/Order.java
@@ -0,0 +1,7 @@
+public class Order {
+    public int orderId;
+    public Customer customer;
+    public double total;
+
+    public void printOrder(){}
+}
