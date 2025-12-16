3eee83d244cbde2d3dbb546fd3c3a512726431b3
Vũ Minh Trung <167429293+trung1609@users.noreply.github.com>
2025-12-16 13:22:29 +0700

    Add files via upload

---
+++ gioi2.java
@@ -0,0 +1,13 @@
+package session07;
+
+public class gioi2 {
+    public String title;
+    public String author;
+    public double price;
+
+    public void printInfo(){
+        System.out.println("Title: "+title);
+        System.out.println("Author: "+author);
+        System.out.println("Price: "+price);
+    }
+}
